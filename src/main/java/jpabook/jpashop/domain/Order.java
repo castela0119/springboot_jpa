3c5fb56edd6ce04ff000c336a326bc571fcc882b
USER <castela0119@gmail.com>
2022-11-25 10:37:47 +0900

    6.1 주문, 주문상품 엔티티 개발, 6.2 주문 레포지토리 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -13,7 +13,8 @@ import java.util.List;
 @Getter @Setter
 public class Order {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "order_id")
     private Long id;
 
@@ -48,6 +49,70 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //==생성 메서드==//
+    /**
+     * 생성할때부터 createOrder 를 호출한다.
+     * member, delivery, orderItems 을 넣어가지고 생성 메서드에서 완결시킨다.
+     * 주문 생성에 대한 비즈니스 로직을 여기에 다 응집해놓음.
+     * 주문 생성과 관련된 것은 여기만 보고 고치면 된다.
+     */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        
+        // 이미 배송된 상품은 주문 취소하지 못한다는 비즈니스 로직이 엔티티 안에 있다.
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        // validation 통과하면 order 의 상태를 CANCEL 로 바꿔주자.
+        this.setStatus(OrderStatus.CANCEL);
+
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+
+//        for (OrderItem orderItem : orderItems) {
+//
+//            // 주문할때 orderItem 에 있는 totalPrice 를 가져온다.
+//            // 주문할때 주문 가격(orderPrice)와 수량(count)가 있기 때문.
+//            totalPrice += orderItem.getTotalPrice();
+//        }
+
+        // 아래 처럼 바꿀 수 있다. (JAVA8)
+        int totalPrice = orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+
+        return totalPrice;
+    }
+
 
 }
 
